64d15a72e5f232ffe04ebaecd3ecd8705d40343a
Mikhail <mishafrolov882000@gmail.com>
2021-04-17 17:24:25 +0600

    Initial Commit

---
+++ SplitBillsApp/SplitBillsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillsAppApp.swift
+//  SplitBillsApp
+//
+//  Created by Mikhail Frolov on 2021-04-17.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
